7592570a943dbc551ee6069d08ee0b444bd95879
Kurt Strecker <ksphotopro@gmail.com>
2018-07-09 12:40:18 -0400

    working and tested name parsing from path

--- src/style_splitter.py
+++ src/style_splitter.py
@@ -4,6 +4,25 @@ class StyleSplitter:
 
     def __init__(self, source_path):
         self.path = source_path
+        self.names = []
+
+    def parse_names(self):
+        file_name = os.path.split(self.path)
+        expected_names = file_name[1].split('-')
+        for name in expected_names:
+            if name[-4:] != ".jpg":
+                full_name = name + ".jpg"
+            else:
+                full_name = name
+            self.names.append(full_name)
+
+
 
     def say_hello(self, msg):
         return msg
+
+if __name__ == '__main__':
+
+    example_path = r'G:\current_coding_projects\style_splitter\sample_data\7SWPO00176TOBYSWEATER_2-W2550O137WHTJUSTINE_2-SVB46MSS18SKY_ALT_1.jpg'
+    example = StyleSplitter(example_path)
+    example.parse_names()
--- style_splitter_tests.py
+++ style_splitter_tests.py
@@ -1,17 +1,24 @@
+import os
 import unittest
 
 from src import style_splitter
 
 class StyleSplitterTestCase(unittest.TestCase):
     def setUp(self):
-        self.style_splitter = style_splitter.StyleSplitter()
+        self.expected_names = ['7SWPO00176TOBYSWEATER_2.jpg', 'W2550O137WHTJUSTINE_2.jpg', 'SVB46MSS18SKY_ALT_1.jpg']
+        self.style_splitter = style_splitter.StyleSplitter(r'G:\current_coding_projects\style_splitter\sample_data\7SWPO00176TOBYSWEATER_2-W2550O137WHTJUSTINE_2-SVB46MSS18SKY_ALT_1.jpg')
 
     def tearDown(self):
         self.style_splitter = None
 
 class StyleSplitterPathTest(StyleSplitterTestCase):
     def runTest(self):
-        self.assertTrue(os.path.exists(self.style_splitter.path))
+        self.assertTrue(os.path.isfile(self.style_splitter.path))
+
+class StyleSplitterNameParsingTest(StyleSplitterTestCase):
+    def runTest(self):
+        self.style_splitter.parse_names()
+        self.assertEquals(self.style_splitter.names, self.expected_names)
 
 if __name__ == '__main__':
     unittest.main()
