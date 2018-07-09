a96d104a72269ac9364840baf43fb5cf0f180136
Kurt Strecker <ksphotopro@gmail.com>
2018-07-09 13:09:40 -0400

    initial test suite creation, tests run all pass

--- style_splitter_tests.py
+++ style_splitter_tests.py
@@ -9,16 +9,20 @@ class StyleSplitterTestCase(unittest.TestCase):
         self.style_splitter = style_splitter.StyleSplitter(r'G:\current_coding_projects\style_splitter\sample_data\7SWPO00176TOBYSWEATER_2-W2550O137WHTJUSTINE_2-SVB46MSS18SKY_ALT_1.jpg')
 
     def tearDown(self):
+        self.expected_names = None
         self.style_splitter = None
 
-class StyleSplitterPathTest(StyleSplitterTestCase):
-    def runTest(self):
+    def test_style_splitter_path_exists(self):
         self.assertTrue(os.path.isfile(self.style_splitter.path))
 
-class StyleSplitterNameParsingTest(StyleSplitterTestCase):
-    def runTest(self):
+    def test_style_splitter_incorrect_path_handling(self):
+        pass
+
+    def test_style_splitter_name_parsing(self):
         self.style_splitter.parse_names()
         self.assertEquals(self.style_splitter.names, self.expected_names)
 
-if __name__ == '__main__':
-    unittest.main()
+suite = unittest.TestLoader().loadTestsFromTestCase(StyleSplitterTestCase)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+
