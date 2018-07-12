7cd7c3c2a7d51871128932b8ee22e2dc43314d08
Kurt Strecker <ksphotopro@gmail.com>
2018-07-12 13:44:37 -0400

    test esape removal

--- src/ui.py
+++ src/ui.py
@@ -13,7 +13,7 @@ class UI:
     def __init__(self):
         print(self.ask_for_input)
         self.raw_target_file_path = raw_input(self.arrow)
-        self.target_file_path = self.raw_target_file_path.strip().strip("'")
+        self.target_file_path = self.raw_target_file_path.strip().strip("'").translate(None, '\\')
         print(self.target_file_path)
         if self.target_file_path == "X".lower():
             self.force_quit = True
